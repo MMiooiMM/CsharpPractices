546ad99c8c128343ccbe10bfc1078689d748eec4
mio <mio093611@gmail.com>
2019-05-17 10:53:42 +0800

    Practice 1 解答

--- CsharpPractices/Practice01/Work.cs
+++ CsharpPractices/Practice01/Work.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
+using System.IO;
 
 namespace CsharpPractices
 {
@@ -10,7 +12,19 @@ namespace CsharpPractices
     {
         public static void Mapping<T>(T entity, string sourceFile, string targetFile)
         {
-
+            var source = string.Empty;
+            using (var read = new StreamReader(sourceFile))
+            {
+                source = read.ReadToEnd();
+            }            
+            foreach(var prop in entity.GetType().GetProperties())
+            {
+                source = source.Replace("@" + prop.Name.ToString(), prop.GetValue(entity).ToString());
+            }
+            using (var write = new StreamWriter(targetFile))
+            {
+                write.Write(source);
+            }
         }
     }
 }
