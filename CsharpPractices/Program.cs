c2d0ff83aa2dcf592e3f1b533979b2324d8ec56b
mio <mio093611@gmail.com>
2019-05-17 10:52:57 +0800

    題目修正

--- CsharpPractices/Program.cs
+++ CsharpPractices/Program.cs
@@ -12,11 +12,11 @@ namespace CsharpPractices
         {
             Work.Mapping(new Class1 { A1 = "1", A2 = "2", A3 = "3", A4 = "4", A5 = "5", A6 = "6", A7 = "7" },
                 System.IO.Directory.GetCurrentDirectory().Replace("\\bin\\Debug", "\\Practice01\\Txt1.txt"),
-                System.IO.Directory.GetCurrentDirectory() + "Txt1.txt");
+                System.IO.Directory.GetCurrentDirectory() + "\\Txt1.txt");
 
             Work.Mapping(new Class2 { B1 = "A", B2 = "B", B3 = "C", B4 = "D", B5 = "E" },
               System.IO.Directory.GetCurrentDirectory().Replace("\\bin\\Debug", "\\Practice01\\Txt2.txt"),
-              System.IO.Directory.GetCurrentDirectory() + "Txt2.txt");
+              System.IO.Directory.GetCurrentDirectory() + "\\Txt2.txt");
         }
     }
 }
